8fef48044fb8303deb73d467a8f0baf9f76f7fbd
Kavadi-Anil <Kavadianil5@gmail.com>
2025-06-15 11:13:05 +0530

    Update fucntions.py

--- fucntions.py
+++ fucntions.py
@@ -11,7 +11,17 @@ increment(list_z)
 print(list_z) 
 #ouput   
 [1, 2, 1]
-[1, 2] 
+[1, 2]  
+
+def increment(list_z):
+    list_z+=[1] 
+    print(list_z) 
+list_z=[1,2]    
+increment(list_z)  
+print(list_z) 
+#ouput   
+[1, 2, 1]
+[1, 2,1] 
 
 
 
